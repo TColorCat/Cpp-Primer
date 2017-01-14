c78ef05e86c64f074c6693bd183394c483a0e64f
TColorCat <2801714044@qq.com>
2017-01-14 21:55:30 +0800

    Create practices 19.3.cpp

---
+++ ch19/practices 19.3.cpp
@@ -0,0 +1,19 @@
+class A{ public:A() = default;virtual ~A(){} };
+	class B :public A{ public:B() = default; ~B(){} };
+	class C :public A{ public:C() = default; ~C(){} };
+	class D :public B, public A{ public:D() = default; ~D(){} };
+	//dynamic_cast 只能从派生类到基类的指针或者引用类型转换
+void test()
+	{
+		A *pa = new D;
+		A*c = new C;
+		B*pd = new B;
+
+		B*ce = dynamic_cast<B*>(c);
+		//NULL
+
+		C*pc = dynamic_cast<C*>(pd);
+		//NULL
+		B* se = dynamic_cast<B*>(pa);
+		//right
+	}
